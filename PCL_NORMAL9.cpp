2283585617a143e9efd09b8d97613420f7b227de
SeriouslyHAO <46627631+SeriouslyHAO@users.noreply.github.com>
2019-02-13 19:15:58 +0800

    Update PCL_NORMAL9.cpp

--- PCL_NORMAL9.cpp
+++ PCL_NORMAL9.cpp
@@ -11,19 +11,17 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main(int argc, char** argv)
 {
 
-
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    if ( pcl::io::loadPCDFile <pcl::PointXYZ> ("test4.pcd", *cloud) == -1 )
+    if ( pcl::io::loadPCDFile <pcl::PointXYZ> ("test_one.pcd", *cloud) == -1 )
     {
         PCL_ERROR("Couldn't read file mypointcloud.pcd\n");
         return -1;
     }
     std::cerr << "cloud ok" << std::endl;
-
-
 
 
     pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
@@ -32,7 +30,7 @@ int main(int argc, char** argv)
     tree->setInputCloud(cloud);
     n.setInputCloud(cloud);
     n.setSearchMethod(tree);
-    n.setKSearch(20);
+    n.setKSearch(100);
     n.compute(*normals);
     std::cerr << "normals ok" << std::endl;
 
@@ -41,26 +39,21 @@ int main(int argc, char** argv)
     pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals(new pcl::PointCloud<pcl::PointNormal>);
     pcl::concatenateFields(*cloud, *normals, *cloud_with_normals);
     pcl::search::KdTree<pcl::PointNormal>::Ptr tree2(new pcl::search::KdTree<pcl::PointNormal>);
-
     tree2->setInputCloud(cloud_with_normals);
-
-
     pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
     pcl::PolygonMesh triangles;
 
-    gp3.setSearchRadius(25);
-    gp3.setMu(2.5);
+    gp3.setSearchRadius(10000.0f);
+    gp3.setMu(10000.0f);
     gp3.setMaximumNearestNeighbors(100);
     gp3.setMaximumSurfaceAngle(M_PI / 2);
     gp3.setMinimumAngle(M_PI / 18);
-    gp3.setMaximumAngle(2 * M_PI / 3);
+    gp3.setMaximumAngle(2 * M_PI / 1);
     gp3.setNormalConsistency(false);
     gp3.setInputCloud(cloud_with_normals);
     gp3.setSearchMethod(tree2);
     gp3.reconstruct(triangles);
     std::cerr << "triangles ok" << std::endl;
-
-
 
     pcl::io::saveVTKFile("mymesh.vtk", triangles);
     std::vector<int> parts = gp3.getPartIDs();
@@ -80,25 +73,23 @@ int main(int argc, char** argv)
         }
     }
 
-
     std::cerr << "viewer show ........" << std::endl;
     boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
-    viewer->setBackgroundColor(0.5,0.5,1.0);
+    viewer->setBackgroundColor(0.4,0.4,0.88);
     viewer->addPolygonMesh(triangles, "my");
-    viewer->setRepresentationToSurfaceForAllActors(); //panel
+//    viewer->setRepresentationToSurfaceForAllActors(); //panel
 //    viewer->setRepresentationToPointsForAllActors(); //cloud
 //    viewer->addPointCloud<pcl::PointXYZ>(cloud, "cloud");
 //    viewer->addPointCloudNormals<pcl::PointXYZ, pcl::Normal>(cloud, normals, 1, 0.03, "normals");//normals
-//    viewer->setRepresentationToWireframeForAllActors();  //line
-
-    viewer->addCoordinateSystem(0.1);
+    viewer->setRepresentationToWireframeForAllActors();  //line
+    viewer->addCoordinateSystem(10.0);
     viewer->initCameraParameters();
+
     while (!viewer->wasStopped())
     {
         viewer->spinOnce(100);
         boost::this_thread::sleep(boost::posix_time::microseconds(100000));
     }
-
 
     // Finish
     return 0;
